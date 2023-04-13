9ac4a95feb084b5e8095907ce7ac6d2604043724
adityapermono8 <adityapermono8@gmail.com>
2023-04-13 10:41:51 +0700

    Menambahkan fungsi input untuk menerima elemen array dari pengguna.

--- LinearSearch_052/LinearSearch_052.cpp
+++ LinearSearch_052/LinearSearch_052.cpp
@@ -7,3 +7,28 @@ int n;			// Number of elements in the array
 int i;			// Index of array elemen
 
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+	// Accept array elements
+	cout << "\n----------------------\n";
+	cout << "\enter array elements \n";
+	cout << "----------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
+
+
+
